b9a9718e202bcfaad4e40de52d0488f99c6af1d8
hp <hp@DESKTOP-FKTABFC>
2020-05-30 09:54:09 +0300

    a commit

---
+++ BurgersUIApp/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using BurgersUIApp.ViewModels;
+using Xamarin.Forms;
+
+namespace BurgersUIApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MenuViewModel();
+        }
+
+        private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Navigation.PushAsync(new MenuDetailPage(), true);
+        }
+    }
+}
